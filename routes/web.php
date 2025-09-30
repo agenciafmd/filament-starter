52dfe5d3ea50bb8bc2cb73bf09b39e9666d57f13
Irineu Junior <irineu@fmd.ag>
2025-09-30 18:24:10 -0300

    rector(código): adicionar declarações de tipo estrito e classes finais
    Este commit adiciona a declaração de tipo estrito em vários arquivos PHP
    e altera algumas classes para serem finais, melhorando a segurança e a
    manutenibilidade do código. Além disso, ajusta o uso de classes e funções
    para seguir as melhores práticas de codificação.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn (): View|Factory => view('welcome'));
+Route::get('/', static fn (): View|Factory => view('welcome'));
