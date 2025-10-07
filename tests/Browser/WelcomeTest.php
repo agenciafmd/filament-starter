a3fd7099fec4094b8d5cde2e22e93953ba59b9b4
Irineu Junior <irineu@fmd.ag>
2025-10-07 14:39:34 -0300

    style(css, js, tests, json): ajustar formatação e adicionar scripts de lint
    Este commit realiza ajustes na formatação de arquivos CSS e JS,
    substituindo aspas simples por aspas duplas. Além disso, adiciona
    scripts de lint no package.json e composer.json para garantir a
    qualidade do código.
    
    Inclui também uma verificação no teste de boas-vindas para evitar
    capturas de tela em ambientes não locais.

--- tests/Browser/WelcomeTest.php
+++ tests/Browser/WelcomeTest.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 it('has welcome page', function (): void {
     $page = visit('/');
-    $page->screenshot();
+
+    if (! app()->environment('local')) {
+        $page->screenshot();
+    }
 
     $page->assertSee('Laravel');
 });
