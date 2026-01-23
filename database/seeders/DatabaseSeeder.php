2ee7edebc1c2a8e08b1d588878f7c39e2fb05ee5
Irineu Junior <irineu@fmd.ag>
2026-01-23 14:30:31 -0300

    chore(dependencies): atualizar dependências do projeto
    Atualiza as versões das dependências no arquivo composer.json e
    package.json, incluindo melhorias e correções de bugs nas bibliotecas
    utilizadas.
    
    Inclui a adição do arquivo _ide_helper_models.php para auxiliar na
    geração de PHPDocs para modelos Eloquent.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,22 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use Agenciafmd\Admix\Models\User;
-use Agenciafmd\Articles\Database\Seeders\ArticleSeeder;
+//use Agenciafmd\Articles\Database\Seeders\ArticleSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 final class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
+        /* remove files from storage/public/fake */
+        @rmdir(public_path('storage/fake'));
+
+        Schema::disableForeignKeyConstraints();
+
         User::factory(100)
             ->create();
 
@@ -28,7 +31,9 @@ final class DatabaseSeeder extends Seeder
             ]);
 
         $this->call([
-            ArticleSeeder::class,
+//            ArticleSeeder::class,
         ]);
+
+        Schema::enableForeignKeyConstraints();
     }
 }
