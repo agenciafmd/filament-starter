3de3f6964663332fbb901e15d5e49d660ccea7b7
Irineu Junior <irineu@fmd.ag>
2026-02-10 14:23:36 -0300

    refactor(js): adicionar limpeza de hooks Livewire em colunas interativas
    Inclui lógica para remover hooks do Livewire na destruição de componentes de colunas do Filament, garantindo melhor gerenciamento de memória e desempenho.

---
+++ config/filament-admix.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Agenciafmd\Postal\PostalPlugin;
+
+return [
+    'schedule' => [
+        'minutes' => mb_substr(base_convert(env('APP_NAME'), 36, 5), 0, 2),
+    ],
+    'timestamp' => [
+        'format' => env('ADMIX_TIMESTAMP_FORMAT', 'd/m/Y H:i:s'),
+    ],
+    'plugins' => [
+        PostalPlugin::class,
+    ],
+];
