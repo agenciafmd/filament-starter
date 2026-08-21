36c7c259d7f8e01086be14565ec8df63e5a9f437
Irineu Junior <irineu@fmd.ag>
2026-08-21 10:37:25 -0300

    feat(config): ajuste no cálculo 'minutes' e plugins ativos

--- config/filament-admix.php
+++ config/filament-admix.php
@@ -3,18 +3,36 @@
 declare(strict_types=1);
 
 use Agenciafmd\Articles\ArticlesPlugin;
+use Agenciafmd\Banners\BannersPlugin;
+use Agenciafmd\BigNumbers\BigNumbersPlugin;
+use Agenciafmd\Faqs\FaqsPlugin;
+use Agenciafmd\HttpLogs\HttpLogsPlugin;
+use Agenciafmd\Leads\LeadsPlugin;
+use Agenciafmd\Partners\PartnersPlugin;
+use Agenciafmd\Postal\PostalPlugin;
+use Agenciafmd\Redirects\RedirectsPlugin;
+use Agenciafmd\Testimonials\TestimonialsPlugin;
 use Filament\Support\Colors\Color;
 use Illuminate\Support\Str;
 
 return [
     'schedule' => [
-        'minutes' => mb_substr(base_convert(Str::slug(env('APP_NAME', 'FMD')), 36, 5), 0, 2),
+        'minutes' => sprintf('%02d', abs(crc32(env('APP_NAME', 'FMD'))) % 60),
     ],
     'timestamp' => [
         'format' => env('ADMIX_TIMESTAMP_FORMAT', 'd/m/Y H:i:s'),
     ],
     'plugins' => [
-        //        ArticlesPlugin::class,
+        ArticlesPlugin::class,
+        BannersPlugin::class,
+        BigNumbersPlugin::class,
+        HttpLogsPlugin::class,
+        LeadsPlugin::class,
+        PartnersPlugin::class,
+        PostalPlugin::class,
+        RedirectsPlugin::class,
+        TestimonialsPlugin::class,
+        FaqsPlugin::class,
     ],
     'colors' => [
         'primary' => Color::Slate,
